86e6f9f8c006ba2380d7d9068c05d3e4d71ec354
lfmaster780 <lfilipe780@gmail.com>
2019-11-26 11:06:31 -0300

    MLP Pronta

--- Projeto2/solucaoA.py
+++ Projeto2/solucaoA.py
@@ -4,20 +4,21 @@ import torch
 from skimage.io import imshow
 import matplotlib.pyplot as plt
 
-caminhosFlamingo = os.listdir("flamingo/")
+
+caminhosFlamingo = os.listdir("C:/Users/Windows/Desktop/RP/RP-2019.2/Projeto2/flamingo/")
 flamingos = []
 
 from skimage.io import imread
 for k in caminhosFlamingo:
 
-    flamingos.append(imread("flamingo/"+k, True))
+    flamingos.append(imread("C:/Users/Windows/Desktop/RP/RP-2019.2/Projeto2/flamingo/"+k, True))
 
-caminhosCanguru = os.listdir("kangaroo/")
+caminhosCanguru = os.listdir("C:/Users/Windows/Desktop/RP/RP-2019.2/Projeto2/kangaroo/")
 cangurus = []
 
 for k in caminhosCanguru:
 
-    cangurus.append(imread("kangaroo/"+k, True))
+    cangurus.append(imread("C:/Users/Windows/Desktop/RP/RP-2019.2/Projeto2/kangaroo/"+k, True))
 
 
 from skimage.transform import rescale, resize, downscale_local_mean
@@ -64,8 +65,62 @@ for canguru in cangurus:
 
     can.append(chave)
 
-print(len(fla),len(can))
+import torch.nn as nn
 
-import torch
-from torch.autograd import Variable
-import torch.nn.functional as F
+train_size_fla = int((len(fla) * 60)/100)
+train_size_cang = int((len(can) * 60)/100)
+train_set = []
+weights = []
+for k in range(train_size_fla):
+    train_set.append(fla[k])
+    weights.append([1.0])
+for k in range(train_size_cang):
+    train_set.append(can[k])
+    weights.append([0.0])
+
+n_in, n_h, n_out, batch_size = 250, 5, 1, train_size_fla + train_size_cang
+y = torch.tensor(weights)
+x = torch.tensor(train_set)
+model = nn.Sequential(nn.Linear(250, n_h),
+                     nn.ReLU(),
+                     nn.Linear(n_h, n_out),
+                     nn.Sigmoid())
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+for epoch in range(1000):
+    # Forward Propagation
+    y_pred = model(x)
+    # Compute and print loss
+    loss = criterion(y_pred, y)
+    print('epoch: ', epoch,' loss: ', loss.item())
+    # Zero the gradients
+    optimizer.zero_grad()
+
+    # perform a backward pass (backpropagation)
+    loss.backward()
+
+    # Update the parameters
+    optimizer.step()
+"""
+test_set = []
+for k in range(train_size_fla+1, len(fla)):
+    test_set.append(fla[k])
+for k in range(train_size_cang+1, len(can)):
+    test_set.append(can[k])
+"""
+checados = 0
+corretos = 0
+for k in range(train_size_fla+1, len(fla)):
+    teste = torch.tensor([fla[k]])
+    print("Predict flamingo", 1.0, model(teste).data[0][0] > 0.5)
+    checados += 1
+    if(model(teste).data[0][0] > 0.5):
+        corretos +=1
+for k in range(train_size_cang+1, len(can)):
+    teste = torch.tensor([can[k]])
+    print("Predict canguru", 1.0, model(teste).data[0][0] < 0.5)
+    checados += 1
+    if(model(teste).data[0][0] < 0.5):
+        corretos +=1
+print(corretos)
+print(checados)
